d130e8ea3319187dde88c125369e07e0a4cde3e4
Raditya2850 <146543619+Raditya2850@users.noreply.github.com>
2024-05-13 21:04:00 +0700

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,34 @@
+document.querySelectorAll('.btnDetail').forEach(item => {
+    item.addEventListener('click', (e) => {
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let nomer = parent.querySelector('.nomer').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML:'<i>Tidak ada informasi yang tersedia</i>';
+        console.log(`deskripsi: ${deskripsi}`)
+
+
+
+        let tombolModal = document.querySelector('.btnModal');
+        tombolModal.click();
+
+
+        document.querySelector('.modalTitle').innerHTML = judul;
+        let image = document.createElement('img');
+        image.src = gambar;
+        image.classList.add('w-100');
+        document.querySelector('.modalImage').innerHTML = '';
+        document.querySelector('.modalImage').appendChild(image);
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+        document.querySelector('.modalHarga').innerHTML = harga;
+
+        const nohp = '6287864766667'
+        let pesan = `https://api.whatsapp.com/send?phone=${nomer}&text=Halo kak, saya mau pesan produk ini ${gambar}`;
+
+        document.querySelector('.btnBeli').href = pesan;
+
+    })
+
+});
